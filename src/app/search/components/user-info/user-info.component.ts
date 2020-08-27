40c0b8531c58b5b08628e659b3491c82b04dc35b
German Quinteros <german.quinteros.1993@gmail.com>
2020-08-27 15:21:18 -0300

    Display Users

--- src/app/search/components/user-info/user-info.component.ts
+++ src/app/search/components/user-info/user-info.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { GithubUser } from '../../model/github-user.model';
+import { Component, Input } from '@angular/core';
+import { GithubUser, GithubUserProfile } from '../../model/github-user.model';
+import { GithubService } from '../../github.service';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.scss']
 })
-export class UserInfoComponent implements OnInit {
-  @Input() githubUser: GithubUser;
-  constructor() { }
-
-  ngOnInit(): void {
+export class UserInfoComponent {
+  public githubUserProfile: GithubUserProfile;
+  @Input() public set githubUser(value: GithubUser) {
+    if(value) {
+      this.getUserProfile(value);
+    }
   }
 
+  constructor(private githubSearch: GithubService) { }
+
+  private getUserProfile(value: GithubUser): void {
+    this.githubSearch.searchUserPofile(value.url).subscribe(
+      (userProfile) => {
+        this.githubUserProfile = userProfile;
+      }
+    );
+  }
 }
--- src/app/search/github.service.ts
+++ src/app/search/github.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from '@environments/environment';
 import { GithubResult } from './model/github-result.model';
 import { first } from 'rxjs/operators';
+import { GithubUserProfile } from './model/github-user.model';
 
 @Injectable()
 export class GithubService {
@@ -12,5 +13,9 @@ export class GithubService {
 
   public searchUser(query: string): Observable<GithubResult> {
     return this.http.get<GithubResult>(`${environment.GITHUB_API}/search/users?q=${query}`).pipe(first())
+  }
+
+  public searchUserPofile(url: string): Observable<GithubUserProfile> {
+    return this.http.get<GithubUserProfile>(url).pipe(first())
   }
 }
