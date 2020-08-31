c8921ed2dd3773e4ff2961120cd1f5c9eb894895
German Quinteros <german.quinteros.1993@gmail.com>
2020-08-31 03:49:30 -0300

    Server Config

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+const publicPath = path.resolve(__dirname, '../dist/github-search');
+app.use(express.static(publicPath));
+app.get('/*', function(req,res) {
+    res.sendFile(path.resolve(__dirname, '../dist/github-search/index.html'));
+  }
+);
+  
+app.listen(process.env.PORT, () => {
+  console.log(`App listening in port: ${process.env.PORT}`);
+});
